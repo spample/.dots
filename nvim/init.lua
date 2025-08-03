ed363e0db72ec3058f376c3355f46b53621aa0e5
spample <luis.antonio.gonzalez501@gmail.com>
2025-08-03 05:11:02 -0500

    +nvim plugin-harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.opt.termguicolors = true
+vim.g.mapleader = " "
 
 vim.opt.clipboard = "unnamedplus"
 
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>a", function()
+  require("harpoon.mark").add_file()
+end)
+vim.keymap.set("n", "<leader>s", function()
+  require("harpoon.ui").toggle_quick_menu()
+end)
+vim.keymap.set("n", "<leader>1", function()
+  require("harpoon.ui").nav_file(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+  require("harpoon.ui").nav_file(2)
+end)
