1367f4bb89658468e449c43250300f91d542b790
spample <luis.antonio.gonzalez501@gmail.com>
2026-01-11 01:03:52 -0600

    +qml nvim lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,11 +10,23 @@ return {
       -- Blink completion → LSP capabilities
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      -- PlatformIO / C / C++
+      ------------------------------------------------------------------
+      -- C / C++ / PlatformIO
+      ------------------------------------------------------------------
       lspconfig.clangd.setup({
         cmd = { "clangd", "--query-driver=/usr/bin/**" },
         filetypes = { "c", "cpp", "objc", "objcpp" },
         root_dir = util.root_pattern("platformio.ini", ".git"),
+        capabilities = capabilities,
+      })
+
+      ------------------------------------------------------------------
+      -- QML / QtQuick
+      ------------------------------------------------------------------
+      lspconfig.qmlls.setup({
+        cmd = { "qmlls6" }, -- Arch Qt6 binary
+        filetypes = { "qml" },
+        root_dir = util.root_pattern(".qmlls.ini", "qmlproject", ".git"),
         capabilities = capabilities,
       })
     end,
