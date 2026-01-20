270a368e9fe0a3e108d30918150c044a2b463d37
spample <luis.antonio.gonzalez501@gmail.com>
2026-01-20 14:01:26 -0600

    + wpaperd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,30 +2,28 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
+    opts = {
+      servers = {
+        -- Simple servers
+        html = {},
+        cssls = {},
+        jsonls = {},
+      },
+    },
+    config = function(_, opts)
       local lspconfig = require("lspconfig")
       local util = lspconfig.util
-
-      -- Blink completion → LSP capabilities
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      ------------------------------------------------------------------
-      -- C / C++ / PlatformIO
-      ------------------------------------------------------------------
+      -- Manual / advanced servers
       lspconfig.clangd.setup({
         cmd = { "clangd", "--query-driver=/usr/bin/**" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
         root_dir = util.root_pattern("platformio.ini", ".git"),
         capabilities = capabilities,
       })
 
-      ------------------------------------------------------------------
-      -- QML / QtQuick
-      ------------------------------------------------------------------
       lspconfig.qmlls.setup({
-        cmd = { "qmlls6" }, -- Arch Qt6 binary
-        filetypes = { "qml" },
+        cmd = { "qmlls6" },
         root_dir = util.root_pattern(".qmlls.ini", "qmlproject", ".git"),
         capabilities = capabilities,
       })
