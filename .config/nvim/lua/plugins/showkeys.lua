fff03c216ca71e4ff0849e9dd53e50064e680535
spample <luis.antonio.gonzalez501@gmail.com>
2026-08-17 12:13:51 -0500

    showkeys plugin starts enabled by default

--- .config/nvim/lua/plugins/showkeys.lua
+++ .config/nvim/lua/plugins/showkeys.lua
@@ -11,6 +11,14 @@ require('showkeys').setup({
   position = 'bottom-right',
 })
 
+-- Automatically turn on showkeys when Neovim UI is ready
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require('showkeys').toggle()
+  end,
+  desc = "Enable Showkeys by default on startup",
+})
+
 -- Manual toggle, useful for demos or checking keybinds.
 vim.keymap.set('n', '<leader>uk', function()
   require('showkeys').toggle()
